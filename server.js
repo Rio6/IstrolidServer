d221453dca312a57ac46d9779639881aa721dc70
Rio6 <rio6.liu@gmail.com>
2018-10-17 21:13:28 -0700

    Proxy sends server informations to client

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var http = require('http');
 var url = require('url');
 require('./fix');
 
+const ADDR = 'localhost'
 const PORT = 8080;
 const ROOT_ADDR = 'ws://198.199.109.223:88'
+
+const SERVER_NAME = 'R26';
+const SERVER_PATH = '/server';
 
 var Istrolid = require('./istrolid.js');
 var WebSocket = require('ws');
@@ -19,6 +23,7 @@ global.Server = function() {
     var rootWss = new WebSocket.Server({noServer: true});
 
     var players = {};
+    var info = {};
 
     this.send = (player, data) => {
         let packet = sim.zJson.dumpDv(data);
@@ -42,23 +47,40 @@ global.Server = function() {
 
     rootWss.on('connection', (ws, req) => {
         console.log("proxying", req.connection.remoteAddress);
-
         let root = new WebSocket(ROOT_ADDR);
 
         root.on('message', data => {
-            ws.send(data);
+            //console.log("root", data);
+            if(ws.readyState === WebSocket.OPEN) {
+                ws.send(data);
+                if(JSON.parse(data)[0] === 'servers') {
+                    let data = {};
+                    data[SERVER_NAME] = info;
+                    ws.send(JSON.stringify(['serversDiff', data]));
+                }
+            }
         });
         root.on('close', e => {
             ws.close();
         });
 
         ws.on('message', data => {
-            root.send(data);
+            //console.log("client", data.substring(0, 100));
+            if(root.readyState === WebSocket.OPEN)
+                root.send(data);
         });
         ws.on('close', e => {
             root.close();
         });
     });
+
+    rootWss.sendToAll = data => {
+        rootWss.clients.forEach(client => {
+            if(client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(data));
+            }
+        });
+    };
 
     wss.on('connection', (ws, req) => {
         console.log("connection from", req.connection.remoteAddress);
@@ -91,7 +113,7 @@ global.Server = function() {
             rootWss.handleUpgrade(req, socket, head, ws => {
                 rootWss.emit('connection', ws, req);
             });
-        } else if(path === '/server') {
+        } else if(path === SERVER_PATH) {
             wss.handleUpgrade(req, socket, head, ws => {
                 wss.emit('connection', ws, req);
             });
@@ -117,6 +139,34 @@ global.Server = function() {
                     client.send(packet);
                 }
             });
+
+            // Send server info
+            let newInfo = {
+                name: SERVER_NAME,
+                address: "ws://" + ADDR + ":" + PORT + SERVER_PATH,
+                observers: sim.players.length,
+                players: sim.players.map(p => { return {
+                    name: p.name,
+                    side: p.side,
+                    ai: p.ai
+                }}),
+                type: sim.serverType,
+                version: VERSION,
+                state: sim.state
+            };
+            let diffInfo = {};
+            for(let k in newInfo) {
+                if(!simpleEquals(info[k], newInfo[k])) {
+                    diffInfo[k] = newInfo[k];
+                }
+            }
+
+            if(Object.keys(diffInfo).length > 0) {
+                let data = {};
+                data[SERVER_NAME] = diffInfo;
+                rootWss.sendToAll(['serversDiff', data]);
+                info = newInfo;
+            }
         }
     }, 17);
 
