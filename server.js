f55d07b80dad0a8445ae026129bdfccfe7eda655
Rio6 <rio6.liu@gmail.com>
2018-10-17 20:17:33 -0700

    Add a root server proxy

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+var http = require('http');
+var url = require('url');
 require('./fix');
+
+const PORT = 8080;
+const ROOT_ADDR = 'ws://198.199.109.223:88'
 
 var Istrolid = require('./istrolid.js');
 var WebSocket = require('ws');
@@ -9,7 +14,9 @@ sim.lastSimInterval = 0;
 
 global.Server = function() {
 
-    this.wss = new WebSocket.Server({port: 8080});
+    var httpServer = http.createServer();
+    var wss = new WebSocket.Server({noServer: true});
+    var rootWss = new WebSocket.Server({noServer: true});
 
     var players = {};
 
@@ -23,7 +30,9 @@ global.Server = function() {
 
     this.stop = () => {
         console.log("stopping server");
-        this.wss.close();
+        httpServer.close();
+        wss.close();
+        rootWss.close();
         clearInterval(interval);
     };
 
@@ -31,28 +40,64 @@ global.Server = function() {
         console.log(msg);
     };
 
-    this.wss.on('connection', (ws, req) => {
+    rootWss.on('connection', (ws, req) => {
+        console.log("proxying", req.connection.remoteAddress);
+
+        let root = new WebSocket(ROOT_ADDR);
+
+        root.on('message', data => {
+            ws.send(data);
+        });
+        root.on('close', e => {
+            ws.close();
+        });
+
+        ws.on('message', data => {
+            root.send(data);
+        });
+        ws.on('close', e => {
+            root.close();
+        });
+    });
+
+    wss.on('connection', (ws, req) => {
         console.log("connection from", req.connection.remoteAddress);
 
-        ws.id = req.headers['sec-websocket-key'];
+        let id = req.headers['sec-websocket-key'];
 
         ws.on('message', msg => {
             let packet = new DataView(new Uint8Array(msg).buffer);
             let data = sim.zJson.loadDv(packet);
             //console.log(data);
-            if(data[0] === "playerJoin") {
+            if(data[0] === 'playerJoin') {
                 let player = sim.playerJoin(...data);
                 player.ws = ws;
-                players[ws.id] = player;
+                players[id] = player;
                 sim.clearNetState();
             } else if(sim[data[0]]) {
-                sim[data[0]].apply(sim, [players[ws.id],...data.slice(1)]);
+                sim[data[0]].apply(sim, [players[id],...data.slice(1)]);
             }
         });
 
-        ws.on('close', msg => {
-            delete players[ws.id];
+        ws.on('close', e => {
+            delete players[id];
         });
+    });
+
+    httpServer.on('upgrade', (req, socket, head) => {
+        let path = url.parse(req.url).pathname;
+
+        if(path === '/') {
+            rootWss.handleUpgrade(req, socket, head, ws => {
+                rootWss.emit('connection', ws, req);
+            });
+        } else if(path === '/server') {
+            wss.handleUpgrade(req, socket, head, ws => {
+                wss.emit('connection', ws, req);
+            });
+        } else {
+            socket.destroy();
+        }
     });
 
     var interval = setInterval(() => {
@@ -67,13 +112,15 @@ global.Server = function() {
             }
 
             let packet = sim.send();
-            this.wss.clients.forEach(client => {
+            wss.clients.forEach(client => {
                 if(client.readyState === WebSocket.OPEN) {
                     client.send(packet);
                 }
             });
         }
     }, 17);
+
+    httpServer.listen(PORT);
 };
 
 global.server = new Server();
