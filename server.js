301e56f64c1ea248915f5cef777d17fdc1dcb69a
Rio6 <rio6.liu@gmail.com>
2018-10-17 17:41:27 -0700

    Put server code into a class

--- server.js
+++ server.js
@@ -3,50 +3,80 @@ require('./fix');
 var Istrolid = require('./istrolid.js');
 var WebSocket = require('ws');
 
-var wss = new WebSocket.Server({port: 8080});
-
-
 global.sim = new Sim();
-sim.cheatSimInterval = 0;
+sim.cheatSimInterval = -12;
 sim.lastSimInterval = 0;
 
-var players = {};
+global.Server = function() {
 
-wss.on('connection', (ws, req) => {
-    console.log("connection from", req.connection.remoteAddress);
+    this.wss = new WebSocket.Server({port: 8080});
 
-    ws.on('message', msg => {
-        let id = req.headers['sec-websocket-key'];
-        let packet = new DataView(new Uint8Array(msg).buffer);
-        let data = sim.zJson.loadDv(packet);
-        if(data[0] === "playerJoin") {
-            players[id] = sim.playerJoin(...data);
-            sim.clearNetState();
-        } else if(sim[data[0]]) {
-            sim[data[0]].apply(sim, [players[id],...data.slice(1)]);
+    var players = {};
+
+    this.send = (player, data) => {
+        let packet = sim.zJson.dumpDv(data);
+        let client = player.ws;
+        if(client && client.readyState === WebSocket.OPEN) {
+            client.send(packet);
         }
-    });
-});
+    };
 
-setInterval(() => {
-    let rightNow = now();
-    if (sim.lastSimInterval + 1000 / 16 + sim.cheatSimInterval <= rightNow) {
-        sim.lastSimInterval = rightNow;
+    this.stop = () => {
+        console.log("stopping server");
+        this.wss.close();
+        clearInterval(interval);
+    };
 
-        if(!sim.paused) {
-            sim.simulate();
-        } else {
-            sim.startingSim();
-        }
+    this.say = msg => {
+        console.log(msg);
+    };
 
-        let packet = sim.send();
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(packet);
+    this.wss.on('connection', (ws, req) => {
+        console.log("connection from", req.connection.remoteAddress);
+
+        ws.id = req.headers['sec-websocket-key'];
+
+        ws.on('message', msg => {
+            let packet = new DataView(new Uint8Array(msg).buffer);
+            let data = sim.zJson.loadDv(packet);
+            //console.log(data);
+            if(data[0] === "playerJoin") {
+                let player = sim.playerJoin(...data);
+                player.ws = ws;
+                players[ws.id] = player;
+                sim.clearNetState();
+            } else if(sim[data[0]]) {
+                sim[data[0]].apply(sim, [players[ws.id],...data.slice(1)]);
             }
         });
-    }
-}, 17);
+
+        ws.on('close', msg => {
+            delete players[ws.id];
+        });
+    });
+
+    var interval = setInterval(() => {
+        let rightNow = now();
+        if(sim.lastSimInterval + 1000 / 16 + sim.cheatSimInterval <= rightNow) {
+            sim.lastSimInterval = rightNow;
+
+            if(!sim.paused) {
+                sim.simulate();
+            } else {
+                sim.startingSim();
+            }
+
+            let packet = sim.send();
+            this.wss.clients.forEach(client => {
+                if(client.readyState === WebSocket.OPEN) {
+                    client.send(packet);
+                }
+            });
+        }
+    }, 17);
+};
+
+global.server = new Server();
 
 var repl = require('repl');
 var ctx = repl.start().context;
